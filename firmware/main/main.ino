cb901fc3b4b2a4dc298108f9318712237a158d70
Virgile Devolder <virgile.devolder@student.unamur.be>
2025-11-02 13:22:39 +0100

    feat: anti-rebond

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -3,12 +3,31 @@
 #define LED_PIN 32
 #define BTN_PIN 27
 
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 30;
+
+int lastButtonReading = LOW;
+int stableButtonState = LOW;
+
 void setup() {
+  Serial.begin(115200);
+  
   pinMode(BTN_PIN, INPUT_PULLDOWN);
   setupLED(LED_PIN);
 }
 
 void loop() {
-  int btnState = digitalRead(BTN_PIN);
-  setLED(btnState == HIGH);
+  int reading = digitalRead(BTN_PIN);
+
+  if (reading != lastButtonReading) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    stableButtonState = reading;
+  }
+  
+  setLED(stableButtonState == HIGH);
+
+  lastButtonReading = reading;
 }
